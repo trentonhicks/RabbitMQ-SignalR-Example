86d6bc509af5eace4ca340083a5a887bef38ae76
Trenton Hicks <trentonmhicks@gmail.com>
2021-05-31 14:04:26 -0500

    Added code to send messages to RabbitMQ.

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace Producer
 {
@@ -6,7 +8,37 @@ namespace Producer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "messages",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            Console.Write("Enter message:");
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+
+                SendMessage(channel, message);
+
+                Console.WriteLine("Message sent.\n");
+            }
+        }
+
+        static void SendMessage(IModel channel, string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "messages",
+                                 basicProperties: null,
+                                 body: body);
         }
     }
 }
